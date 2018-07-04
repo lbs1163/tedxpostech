936786072048ff0204990e2ba080f92add18dc6f
lbs1163 <lbs1163@postech.ac.kr>
2018-07-04 17:44:02 +0900

    Make header and footer

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name='index'),
+	path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('pastevent/<int:year>/', views.pastevent, name='pastevent'),
+    path('speakers/', views.speakers, name='speakers'),
+    path('organizers/', views.organizers, name='organizers'),
+    path('sponsors/', views.sponsors, name='sponsors'),
+    path('register/', views.register, name='register'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
 
-def index(request):
-	return render(request, "core/index.html", {})
+def home(request):
+	return render(request, "core/home.html", {})
+
+def about(request):
+    return render(request, "core/about.html", {})
+
+def pastevent(request):
+    return render(request, "core/pastevent.html", {})
+
+def speakers(request):
+    return render(request, "core/speakers.html", {})
+
+def organizers(request):
+    return render(request, "core/organizers.html", {})
+
+def sponsors(request):
+    return render(request, "core/sponsors.html", {})
+
+def register(request):
+    return render(request, "core/register.html", {})
