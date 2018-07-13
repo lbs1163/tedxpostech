2bbcb1db4f11237f16ac93eb3bb2dddbb546ea0b
lbs1163 <lbs1163@postech.ac.kr>
2018-07-14 01:40:52 +0900

    Set DEBUG to False

--- tedxpostech/settings.py
+++ tedxpostech/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '918x5%ho-3sf(wbyen4$l(9w5jv3vd3_7leb&fe-7wx*(8(xa%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'tedx.postech.ac.kr']
 
